73fea510ff6c501f2994e214336f7057bf2e510a
Harsh-Duche <ducheharsh@gmail.com>
2025-07-19 01:10:04 +0530

    init: repo

---
+++ hifi-agent/tools/example_tool.py
@@ -0,0 +1,13 @@
+# example_tool.py
+
+def add_todo(todo: str) -> str:
+    #  db insert todo
+    return todo
+
+def get_todos() -> list[str]:
+    #  db select todos
+    return todos
+
+def delete_todo(todo: str) -> str:
+    #  db delete todo
+    return todo

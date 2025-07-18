ddc1ad2458b9a39f7fc072dc1e8e6ffbf7bd7ea1
Harsh-Duche <ducheharsh@gmail.com>
2025-07-19 01:10:04 +0530

    init: repo

---
+++ hifi-agent/agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents import Agent
+
+hifi_agent = Agent(
+    model='gemini-2.0-flash-001',
+    name='hifi_agent',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge',
+)
---
+++ hifi-agent/sub_agent/trader_agent/agent.py
@@ -0,0 +1,8 @@
+from google.adk.agents import Agent
+
+trader_agent = Agent(
+    model='gemini-2.0-flash-001',
+    name='trader_agent',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge',
+)

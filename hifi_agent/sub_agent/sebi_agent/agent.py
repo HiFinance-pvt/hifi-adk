281ee90aa3e37364b92df8eb71867162e93b450b
Harsh-Duche <ducheharsh@gmail.com>
2025-07-23 00:41:50 +0530

    deploy: hifi adk, updated root, fi_mcp, tax mitra agent

--- hifi_agent/sub_agent/sebi_agent/agent.py
+++ hifi_agent/sub_agent/sebi_agent/agent.py
@@ -1,10 +1,9 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search
+
 
 sebi_agent = Agent(
     model='gemini-2.0-flash-001',
     name='sebi_agent',
     description='You are a sebi agent that is responsible for providing financial advice according to India\'s [SEBI] regulations and India specific financial advice.',
     instruction='You are a sebi agent that is responsible for providing financial advice according to India\'s [SEBI] regulations and India specific financial advice.',
-    tools=[google_search],
 )
